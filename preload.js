f32e25061c61efee6c715136cb0135b96a3e12e8
Jared Miller <jaredmiller219@gmail.com>
2025-07-11 16:44:45 -0400

    works offline now

---
+++ preload.js
@@ -0,0 +1,22 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Terminal methods
+  terminalInit: () => ipcRenderer.send('terminal-init'),
+  terminalInput: (data) => ipcRenderer.send('terminal-input', data),
+  terminalClose: () => ipcRenderer.send('terminal-close'),
+  onTerminalOutput: (callback) => ipcRenderer.on('terminal-output', callback),
+  
+  // File methods
+  openFileDialog: () => ipcRenderer.send('open-file-dialog'),
+  saveFile: (data) => ipcRenderer.send('save-file', data),
+  saveFileDialog: (data) => ipcRenderer.send('save-file-dialog', data),
+  readFile: (data) => ipcRenderer.send('read-file', data),
+  onFileOpened: (callback) => ipcRenderer.on('file-opened', callback),
+  onFileSaved: (callback) => ipcRenderer.on('file-saved', callback),
+  
+  // Remove listeners
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
+});
