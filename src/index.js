3e002df77cd0ed001512277d8b83bc9079953498
Jared Miller <jaredmiller219@gmail.com>
2025-07-15 20:05:35 -0400

    Flatten core structure: move App files from src/core/components/ to src/core/
    - Move App.js and App.css directly into src/core/
    - Remove empty components directory
    - Update import paths in src/index.js
    - Simplify core module structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./core/components/App.css";
-import App from "./core/components/App.js";
+import "./core/App.css";
+import App from "./core/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
